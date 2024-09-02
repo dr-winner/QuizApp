248a3ac23828fc8f2810b533f5ec303420d60cbf
dr-winner <duvorrichardwinner@gmail.com>
2024-09-02 23:40:34 +0000

    more logic for questions selection

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const welcomeText = document.querySelector(".welcomeText");
 const quitQuiz = infoContainer.querySelector(".navButtons .quitQuiz");
 const continueButton = infoContainer.querySelector(".navButtons .continueButton");
 const quizContainer = document.querySelector(".quizContainer");
+const timeCounter = quizContainer.querySelector(".timer .timerSec");
+const timerBar = quizContainer.querySelector("header .timerBar");
 
 const ans_list = document.querySelector(".answerList");
 
@@ -26,11 +28,15 @@ continueButton.onclick = () => {
     quizContainer.classList.add("activeQuiz"); //show the quiz container
     showQuestions(0);
     questionStatus(1);
+    startTimer(15);
+    startTimerLine(0);
 }
 
 let questionCount = 0;
 let quesNumb = 1;
-
+let counter;
+let timeValue = 15;
+let widthValue = 0;
 const nextButton = quizContainer.querySelector(".nextBtn");
 
 // Next button click
@@ -38,11 +44,16 @@ const nextButton = quizContainer.querySelector(".nextBtn");
 nextButton.onclick = () =>{
     if(questionCount < questions.length - 1){
         questionCount++;
-        quesNumb++
+        quesNumb++;
         showQuestions(questionCount);
-        questionStatus(quesNumb)
+        questionStatus(quesNumb);
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        nextButton.style.display = "none";
     } else{
-        console.log("Test completed!");
+        console.log("Test completed");
     }
     
 }
@@ -61,25 +72,33 @@ function showQuestions(index){
 
     const option = ans_list.querySelectorAll(".option");
     for(let i = 0; i < option.length; i++){
-        option[i].setAttribute("onclick", "optionSelected(this)")
+        option[i].setAttribute("onclick", "optionSelected(this)");
     }
 }
 
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
 function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
     let userAnswer = answer.textContent;
     let correctAnswer = questions[questionCount].answer;
     let allOptions = ans_list.children.length;
     if(userAnswer == correctAnswer){
         answer.classList.add("correct");
         console.log("Answer is correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
     } else{
         answer.classList.add("incorrect");
-        console.log("Answer is incorrect");      
+        console.log("Answer is incorrect"); 
+        answer.insertAdjacentHTML("beforeend", crossIcon);     
         
         // once user selects the wrong answer, auto select the correct answer
         for(let i = 0; i < allOptions; i++){
             if( ans_list.children[i].textContent == correctAnswer){
                 ans_list.children[i].setAttribute("class", "option correct");
+                ans_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
             }            
         }
     }
@@ -88,10 +107,35 @@ function optionSelected(answer){
     for(let i = 0; i < allOptions; i++){
         ans_list.children[i].classList.add("disabled");
     }
+    nextButton.style.display = "block";
 }
 
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCounter.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCounter.textContent;
+            timeCounter.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCounter.textContent = "00";
+        }        
+    }
+}
 
-
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1;
+        timerBar.style.width = time + "px";
+        if(time > 549){            
+            clearInterval(counterLine);
+        }        
+    }
+}
 
 function questionStatus(index){
     const bottomQuestionCounter = quizContainer.querySelector(".questionStat");
